a595a2b833c77bb229f3c96657978b59dc7cd23e
Theon Antwan Beckford <t.a.beckford@se21.qmul.ac.uk>
2025-12-11 14:09:52 +0000

    Same as last

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"assessment/code"
+	"fmt"
 )
 
 func main() {
 
+	fmt.Println("Q1 test output")
 	code.RunQ1()
+	fmt.Print("\n")
+
+	fmt.Println("Q2 test output")
 	code.RunQ2()
+	fmt.Print("\n")
+
+	fmt.Println("Q3 test output")
 	code.RunQ3()
 
 }
